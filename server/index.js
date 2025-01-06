9b596789b61ee88d25046bf1310f8da3978d4dad
rutika khedkar <rutikakhadekar@gmail.com>
2025-01-06 17:31:52 +0530

    added files

--- server/index.js
+++ server/index.js
@@ -1,25 +1,35 @@
 require('dotenv').config();
 
 const express = require('express');
-const mongoose = require('mongoose');
+const bodyParser=require('body-parser')
+const {Server}= require('socket.io')
+
+
+const io = new Server();
 const app = express();
 
-// Environment variables
-const PORT = process.env.PORT || 5000;
-const MONGO_URI = process.env.MONGO_URI;
+app.use(bodyParser.json())
+// const mongoose = require('mongoose');
+// // Environment variables
+// const PORT = process.env.PORT || 5000;
+// const MONGO_URI = process.env.MONGO_URI;
 
-// MongoDB connection
-mongoose.connect(MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+// // MongoDB connection
+// mongoose.connect(MONGO_URI, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+// })
+//   .then(() => console.log('Connected to MongoDB'))
+//   .catch((err) => console.error('MongoDB connection error:', err));
+
+//   app.get('/', (req, res) => {
+//     res.status(200).send("hello");
+//   });
+// // Start server
+
+io.on("connection",(socket)=>{
+  
 })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
-
-  app.get('/', (req, res) => {
-    res.status(200).send("hello");
-  });
-// Start server
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
